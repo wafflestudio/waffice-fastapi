b025396b6c470ae442aa2eb07e21be81bb18769d
Kang Myeong Seok <tomskang@naver.com>
2025-09-26 05:11:58 +0900

    resolve lint problem

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,10 +1,6 @@
 from .user import User, UserBase, UserCreate, UserPrivilege, UserType
-from .user_history import (
-    UserHistory,
-    UserHistoryBase,
-    UserHistoryCreate,
-    UserHistoryType,
-)
+from .user_history import (UserHistory, UserHistoryBase, UserHistoryCreate,
+                           UserHistoryType)
 
 __all__ = [
     "User",
