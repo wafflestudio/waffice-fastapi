385779c635f6675550486462f884364a16bb09be
swfs0417 <apdl0012@gmail.com>
2026-07-28 22:50:03 +0900

    Fix project deletion leader sync order

--- app/services/project.py
+++ app/services/project.py
@@ -542,15 +542,6 @@ class ProjectService:
         """Soft delete a project unless it has a pending approval request."""
         import time
 
-        leader_user_ids = {
-            member.user_id
-            for member in MemberService.list_active(db, project.id)
-            if member.role == MemberRole.LEADER
-        }
-        project.deleted_at = int(time.time())
-        db.flush()
-        for user_id in leader_user_ids:
-            MemberService.sync_leader_flag(db, user_id)
         locked_project = (
             db.query(Project)
             .filter(
@@ -581,7 +572,15 @@ class ProjectService:
         if pending_request_exists:
             raise ProjectHasPendingRequestsError()
 
+        leader_user_ids = {
+            member.user_id
+            for member in MemberService.list_active(db, locked_project.id)
+            if member.role == MemberRole.LEADER
+        }
         locked_project.deleted_at = int(time.time())
+        db.flush()
+        for user_id in leader_user_ids:
+            MemberService.sync_leader_flag(db, user_id)
         db.commit()
 
 
